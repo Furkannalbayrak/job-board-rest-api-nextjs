d5ae249eda2ca047703394b6fbd9dff38fd3242d
Furkan Albayrak <furkanalb42@gmail.com>
2026-02-28 21:59:28 +0300

    admin page.tsx eklendi

---
+++ app/(dashboard)/admin/page.tsx
@@ -0,0 +1,30 @@
+import User from "@/components/User";
+import { authOptions } from "@/lib/auth"
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth"
+
+const page = async () => {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <div>
+      {session?.user ? (
+        <h2>Admin Page - welcome back {session.user.username}</h2>
+      ) : (
+        <div>Please login to see this admin page</div>
+      )}
+
+      {/* Client session çok daha yavaş çalışıyor 
+      Server session ise çok çok hızlı */}
+
+      <h2>Client Session</h2>
+      <User />
+
+      <h2>Server Session</h2>
+      {JSON.stringify(session)}
+
+    </div>
+  )
+}
+
+export default page
