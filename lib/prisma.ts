acd74033e669fd64d645de5435ff5c2d24e379bb
Furkan Albayrak <furkanalb42@gmail.com>
2026-03-01 16:18:28 +0300

    prisma.ts gereksiz prisma baglantilari engellendi

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,16 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient({ adapter });
 
-export { prisma };
+// Eğer development ortamındaysak, prisma örneğini globale kaydet ki 
+// her sayfa yenilendiğinde yeni bir bağlantı açılmasın.
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
