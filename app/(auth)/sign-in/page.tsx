52067c38d54ee68565dbe10e7777f8c5371dbcfe
Furkan Albayrak <furkanalb42@gmail.com>
2026-03-16 19:00:51 +0300

    sign-up ve sign-in page.tsx duzenlendi

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,8 +2,14 @@ import SignInForm from '@/components/form/SignInForm';
 
 const page = () => {
   return (
-    <div className='w-full'>
-      <SignInForm />
+    <div className='min-h-screen bg-gray-50 flex items-center justify-center p-4'>
+      <div className='w-full max-w-md bg-white rounded-2xl shadow-xl p-8 border mb-28 border-gray-100'>
+        <div className="mb-8 text-center">
+          <h1 className="text-3xl font-bold text-gray-900 tracking-tight">Giriş Yap</h1>
+          <p className="text-sm text-gray-500 mt-2">Hesabınıza erişmek için bilgilerinizi girin</p>
+        </div>
+        <SignInForm />
+      </div>
     </div>
   );
 };
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -2,8 +2,16 @@ import SignUpForm from '@/components/form/SignUpForm';
 
 const page = () => {
   return (
-    <div className='w-full'>
-      <SignUpForm />
+    <div className='min-h-screen bg-gray-50 flex items-center justify-center p-10 pb-20'>
+      <div className='w-full max-w-md bg-white rounded-2xl shadow-xl p-8 border border-gray-100'>
+        <div className="mb-8 text-center">
+          <h1 className="text-3xl font-bold text-gray-900 tracking-tight">Kayıt Ol</h1>
+          <p className="text-sm text-gray-500 mt-2">
+            Hemen hesabınızı oluşturun ve ilanlara göz atın
+          </p>
+        </div>
+        <SignUpForm />
+      </div>
     </div>
   );
 };
